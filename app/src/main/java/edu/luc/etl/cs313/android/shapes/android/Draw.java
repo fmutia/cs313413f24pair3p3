aa021b2ee32d6c42ddfd6dc12453f79d61decc43
Nicole Mutia <fmutia@luc.edu>
2024-10-07 09:04:16 -0500

    first two tests passed

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,9 +17,9 @@ public class Draw implements Visitor<Void> {
     private final Paint paint;
 
     public Draw(final Canvas canvas, final Paint paint) {
-        this.canvas = null; // FIXME
-        this.paint = null; // FIXME
-        paint.setStyle(Style.STROKE);
+        this.canvas = canvas; // FIXME
+        this.paint = paint; // FIXME
+        //paint.setStyle(Style.STROKE);
     }
 
     @Override
@@ -30,44 +30,55 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onStrokeColor(final StrokeColor c) {
-
+        paint.setColor(c.getColor());
         return null;
     }
 
     @Override
     public Void onFill(final Fill f) {
-
+        paint.setStyle(Style.FILL_AND_STROKE);
+        f.getShape().accept(this);
         return null;
     }
 
     @Override
     public Void onGroup(final Group g) {
-
+        for (Shape s: g.getShapes()) {
+            s.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void onLocation(final Location l) {
-
+        canvas.translate(l.getX(), l.getY());
+        l.getShape().accept(this);
+        canvas.translate(-l.getX(), -l.getY());
         return null;
     }
 
     @Override
     public Void onRectangle(final Rectangle r) {
-
+        canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
         return null;
     }
 
     @Override
-    public Void onOutline(Outline o) {
-
+    public Void onOutline(final Outline o) {
+        paint.setStyle(Style.STROKE);
+        o.getShape().accept(this);
         return null;
     }
 
     @Override
     public Void onPolygon(final Polygon s) {
+        final float[] pts = new float[s.getPoints().size() * 2];
+        int i = 0;
 
-        final float[] pts = null;
+        for (Point p: s.getPoints()) {
+            pts[i++] = p.getX();
+            pts[i++] = p.getY();
+        }
 
         canvas.drawLines(pts, paint);
         return null;
